ff0032c94d205798588051c15f9156d77d884895
Steven Kreitzer <skre@skre.me>
2024-04-11 07:38:09 -0500

    feat(ollama): build pre release

--- apps/ollama/ci/latest.sh
+++ apps/ollama/ci/latest.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/ollama/ollama/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
+channel=$1
+
+if [[ "${channel}" == "beta" ]]; then
+    version="$(curl -sX GET "https://api.github.com/repos/ollama/ollama/releases" | jq --raw-output 'map(select(.prerelease)) | first | .tag_name' 2>/dev/null)"
+else
+    version="$(curl -sX GET "https://api.github.com/repos/ollama/ollama/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
+fi
+
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
