84f17b87fbd246ab78842c71863d522d59a8f3c4
Steven Kreitzer <skre@skre.me>
2024-04-26 16:24:51 -0500

    fix(open-webui): clean up entrypoint and tests

--- apps/open-webui/entrypoint.sh
+++ apps/open-webui/entrypoint.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
-cd /app/backend
-
-KEY_FILE=.webui_secret_key
-
 PORT="${PORT:-8080}"
-if test "$WEBUI_SECRET_KEY $WEBUI_JWT_SECRET_KEY" = " "; then
-  echo No WEBUI_SECRET_KEY provided
 
-  if ! [ -e "$KEY_FILE" ]; then
-    echo Generating WEBUI_SECRET_KEY
-    # Generate a random value to use as a WEBUI_SECRET_KEY in case the user didn't provide one.
-    echo $(head -c 12 /dev/random | base64) > $KEY_FILE
-  fi
+if [[ -z "$WEBUI_SECRET_KEY" ]]; then
+    echo "WEBUI_SECRET_KEY is not set"
 
-  echo Loading WEBUI_SECRET_KEY from $KEY_FILE
-  WEBUI_SECRET_KEY=`cat $KEY_FILE`
+    KEY_FILE=".webui_secret_key"
+    if [ ! -e "$KEY_FILE" ]; then
+        echo "Generating WEBUI_SECRET_KEY"
+        echo $(head -c 12 /dev/random | base64) > $KEY_FILE
+    fi
+
+    echo "Loading WEBUI_SECRET_KEY from $KEY_FILE"
+    WEBUI_SECRET_KEY=`cat $KEY_FILE`
 fi
 
-WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec uvicorn main:app --host 0.0.0.0 --port "$PORT" --forwarded-allow-ips '*'
+if [[ -z "$LD_LIBRARY_PATH" ]]; then
+    export LD_LIBRARY_PATH="/usr/local/lib/python3.11/site-packages/torch/lib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib/python3.11/site-packages/nvidia/cudnn/lib"
+    echo "Setting LD_LIBRARY_PATH to $LD_LIBRARY_PATH"
+fi
+
+cd /app/backend
+exec \
+    uvicorn main:app \
+        --host 0.0.0.0 \
+        --port "$PORT" \
+        --forwarded-allow-ips "*"
