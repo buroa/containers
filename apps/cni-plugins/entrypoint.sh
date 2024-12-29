73837ec0d3b6f32bdaed4b3848c03544c665aa9e
Steven Kreitzer <skre@skre.me>
2024-12-29 08:32:02 -0700

    fix(cni-plugins): oops forgot bash lol

--- apps/cni-plugins/entrypoint.sh
+++ apps/cni-plugins/entrypoint.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-CNI_DIR="/opt/cni"
-
 # Copy the binary and rename so the move is atomic
 install_cni() {
     src="$1"
     bin_name="$(basename $src)"
     dst="${CNI_DIR}/bin/$bin_name"
 
-    if [ ! -f $dst ]; then
+    if [ ! -f "$dst" ]; then
         tmp_dst="${CNI_DIR}/bin/.$bin_name.new"
         echo "Installing $bin_name to $dst ..."
         cp $src $tmp_dst && \
@@ -18,6 +16,6 @@ install_cni() {
 }
 
 # Install all the CNI plugins
-for file in /cni/*; do
-    install_cni $file || true
+for plugin in /plugins/*; do
+    install_cni $plugin || true
 done
