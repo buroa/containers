0ebdd4e6adad10a26676479d68dc32ba2e2f9a40
Steven Kreitzer <skre@skre.me>
2025-01-05 17:41:43 -0600

    chore(apps): house keeping (#121)

--- apps/maddy/ci/latest.sh
+++ apps/maddy/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/foxcpp/maddy/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
+version="$(curl -sX GET "https://api.github.com/repos/foxcpp/maddy/releases/latest" | jq --raw-output '.tag_name')"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
