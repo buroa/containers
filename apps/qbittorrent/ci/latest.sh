94d445e8a111842c3cfde50efec5c13676bf52a2
Steven Kreitzer <skre@skre.me>
2025-01-06 19:22:45 -0600

    chore(apps): house keeping

--- apps/qbittorrent/ci/latest.sh
+++ apps/qbittorrent/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq --raw-output '.qbittorrent')
+version=$(curl -sL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq --raw-output '.qbittorrent' 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
