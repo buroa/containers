e84371ed432975ac2f3a4fb4699c3ec80ac0bac1
Devin Buhl <onedr0p@users.noreply.github.com>
2025-03-19 08:37:13 -0400

    feat: add transmission (#204)
    Signed-off-by: Devin Buhl <devin@buhl.casa>

---
+++ apps/transmission/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+minijinja-cli --env /settings.json.j2 > /config/settings.json
+
+if [[ "${TRANSMISSION__DEBUG}" == "true" ]]; then
+    echo "Transmission starting with the following configuration..."
+    cat /config/settings.json
+fi
+
+#shellcheck disable=SC2086
+exec \
+    /usr/bin/transmission-daemon \
+        --foreground \
+        --config-dir /config \
+        --port "${TRANSMISSION__RPC_PORT:-9091}" \
+        "$@"
