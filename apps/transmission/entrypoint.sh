c14f2db7b407217845fba5c6628c920cc9215b10
Devin Buhl <devin@buhl.casa>
2025-03-19 08:05:54 -0400

    feat: add transmission
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/transmission/entrypoint.sh
+++ apps/transmission/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update settings.json with environment variables
-envsubst < /app/settings.json.tpl > /config/settings.json
+envsubst < /settings.json.tpl > /config/settings.json
 
 if [[ "${TRANSMISSION__DEBUG}" == "true" ]]; then
     echo "Transmission starting with the following configuration..."
@@ -10,7 +10,7 @@ fi
 
 #shellcheck disable=SC2086
 exec \
-    /app/transmission-daemon \
+    /usr/bin/transmission-daemon \
         --foreground \
         --config-dir /config \
         --port "${TRANSMISSION__RPC_PORT:-9091}" \
