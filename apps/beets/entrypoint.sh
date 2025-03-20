9e1acc51eef72ed7459a94443f21825bc7c53564
Devin Buhl <devin@buhl.casa>
2025-03-20 13:18:18 -0400

    feat: add beets with betanin
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/beets/entrypoint.sh
+++ apps/beets/entrypoint.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 
 export BEETSDIR="/config/.config/beets"
-export FPCALC="/usr/bin/fpcalc"
-export BETANINDIR="/config/.cache/betanin"
-
-if [[ ! -d "${BEETSDIR}" ]]; then
-    mkdir -p "${BEETSDIR}"
-fi
+export BETANINDIR="/config/.config/betanin"
 
 if [[ ! -f "${BEETSDIR}/config.yaml" ]]; then
+    mkdir -p "${BEETSDIR}"
     cp /config-beets.yaml "${BEETSDIR}/config.yaml"
 fi
 
-if [[ ! -d "${BETANINDIR}" ]]; then
+if [[ ! -f "${BETANINDIR}/config.toml" ]]; then
+    printf "Copying over default configuration ...\n"
     mkdir -p "${BETANINDIR}"
+    cp /config-betanin.toml "${BETANINDIR}/config.toml"
+
+    printf "Creating password and api keys for the user 'beets' ...\n"
+    password=$(tr -dc 'a-z0-9' < /dev/urandom | fold -w 32 | head -n 1)
+    api_key=$(tr -dc 'a-z0-9' < /dev/urandom | fold -w 32 | head -n 1)
+    sed -i -e "s/^password *=.*$/password = \"${password}\"/g" "${BETANINDIR}/config.toml"
+    sed -i -e "s/^api_key *=.*$/api_key = \"${api_key}\"/g" "${BETANINDIR}/config.toml"
+    echo "password: ${password}"
+    echo "api-key: ${api_key}"
 fi
 
-if [[ ! -f "${BETANINDIR}/config.toml" ]]; then
-    envsubst < /config-betanin.toml.tpl > "${BETANINDIR}/config.toml"
-fi
+[[ -n "${BETANIN__PASSWORD}" ]] && sed -i -e "s/^password *=.*$/password = \"${BETANIN__PASSWORD}\"/g" "${BETANINDIR}/config.toml"
+[[ -n "${BETANIN__API_KEY}" ]] && sed -i -e "s/^api_key *=.*$/api_key = \"${BETANIN__API_KEY}\"/g" "${BETANINDIR}/config.toml"
 
 exec \
     /usr/local/bin/betanin \
