d4383829a191639ccdac3e77786f2b0b00348f79
Devin Buhl <devin@buhl.casa>
2025-03-20 12:20:47 -0400

    feat: add beets with betanin
    Signed-off-by: Devin Buhl <devin@buhl.casa>

---
+++ apps/beets/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export BEETSDIR="/config/.config/beets"
+export FPCALC="/usr/bin/fpcalc"
+export BETANINDIR="/config/.cache/betanin"
+
+if [[ ! -d "${BEETSDIR}" ]]; then
+    mkdir -p "${BEETSDIR}"
+fi
+
+if [[ ! -f "${BEETSDIR}/config.yaml" ]]; then
+    cp /config-beets.yaml "${BEETSDIR}/config.yaml"
+fi
+
+if [[ ! -d "${BETANINDIR}" ]]; then
+    mkdir -p "${BETANINDIR}"
+fi
+
+if [[ ! -f "${BETANINDIR}/config.toml" ]]; then
+    minijinja-cli --env /config-betanin.toml.j2 > "${BETANINDIR}/config.toml"
+fi
+
+exec \
+    /usr/bin/betanin \
+        "$@"
