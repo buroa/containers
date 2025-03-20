d13542300aef097fdb4b09fca6440d23e4ff3e57
Devin Buhl <devin@buhl.casa>
2025-03-20 13:30:37 -0400

    feat: add beets with betanin
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/beets/entrypoint.sh
+++ apps/beets/entrypoint.sh
@@ -9,17 +9,16 @@ if [[ ! -f "${BEETSDIR}/config.yaml" ]]; then
 fi
 
 if [[ ! -f "${BETANINDIR}/config.toml" ]]; then
-    printf "Copying over default configuration ...\n"
     mkdir -p "${BETANINDIR}"
     cp /config-betanin.toml "${BETANINDIR}/config.toml"
 
-    printf "Creating password and api keys for the user 'beets' ...\n"
     password=$(tr -dc 'a-z0-9' < /dev/urandom | fold -w 32 | head -n 1)
     api_key=$(tr -dc 'a-z0-9' < /dev/urandom | fold -w 32 | head -n 1)
     sed -i -e "s/^password *=.*$/password = \"${password}\"/g" "${BETANINDIR}/config.toml"
     sed -i -e "s/^api_key *=.*$/api_key = \"${api_key}\"/g" "${BETANINDIR}/config.toml"
-    echo "password: ${password}"
-    echo "api-key: ${api_key}"
+    echo "Created password and api keys for the user beets:"
+    echo "Password: ${password}"
+    echo "API Key: ${api_key}"
 fi
 
 [[ -n "${BETANIN__PASSWORD}" ]] && sed -i -e "s/^password *=.*$/password = \"${BETANIN__PASSWORD}\"/g" "${BETANINDIR}/config.toml"
